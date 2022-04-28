d15b20508033b7c34df6ae430c3fbd3e567d9cd8
Anders <don-sbn@hotmail.com>
2022-04-28 12:43:33 +0200

    Fibonacci WIP, tests done

---
+++ src/Datastructures and Algorithms/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures_and_Algorithms
+{
+    public class Fibonacci
+    {
+        /// <summary>
+        /// This is a function that aims to return the number of a given position (n), from the fibonacci sequence.
+        /// Additionally, this function aims to 
+        /// </summary>
+        /// <param name="n">The index of the number from the fibonacci sequence.</param>
+        /// <returns>A number from the fibonacci sequence.</returns>
+        public static int Fib(int n)
+        {
+            if (n <= 2) return 1;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
---
+++ src/TestProject/FibonacciTests.cs
@@ -0,0 +1,26 @@
+﻿using Datastructures_and_Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void SmallInputTests()
+        {
+            Assert.AreEqual(8, Fibonacci.Fib(6));
+            Assert.AreEqual(13, Fibonacci.Fib(7));
+            Assert.AreEqual(21, Fibonacci.Fib(8));
+        }
+
+        [TestMethod]
+        public void LargeInputTests()
+        {
+            Assert.AreEqual(50, Fibonacci.Fib(50));
+        }
+    }
+}
