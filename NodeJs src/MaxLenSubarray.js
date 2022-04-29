c3a6b0bb9c1204b50f402f415fd6efa4ec29606b
Anders <don-sbn@hotmail.com>
2022-04-29 14:44:19 +0200

    set up MaxLenSubarray.js

---
+++ NodeJs src/MaxLenSubarray.js
@@ -0,0 +1,12 @@
+// Given an integer array, find the maximum length contiguous subarray having a given sum.
+
+// Note: Since an input can contain several maximum length subarrays with given sum, 
+// the solution should return any one of the maximum length subarray.
+
+const maxLenSubarray = (numbers, target) => {
+    
+}
+
+// Tests
+console.log(maxLenSubarray([5, 6, -5, 5, 3, 5, 3, -2, 0], 8)) // [-5, 5, 3, 5]
+// Explanation: The subarrays with sum 8 are [[-5, 5, 3, 5], [3, 5], [5, 3]]. The longest subarray is [-5, 5, 3, 5] having length 4.
