5df5406ac31bb503f19527a74bc379a27f69d15e
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2023-03-04 17:44:09 +0100

    Refactored tests

--- NodeJs src/IsPalindrome.js
+++ NodeJs src/IsPalindrome.js
@@ -10,32 +10,21 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+    return false;
 };
 
 // Tests
 const { describe, it } = require('node:test');
-
 var test = require('unit.js');
 var assert = test.assert;
-
 describe('IsPalindrome.js tests', function(){
     it('test_00', function(){
-        const input = 121;
-        const expected = true;
-        const actual = isPalindrome(input);
-        assert(actual, expected, `Expected: ${expected}, Actual: ${actual}`);
+        assert(isPalindrome(121));
     });
     it('test_01', function(){
-        const input = -121;
-        const expected = false;
-        const actual = isPalindrome(input);
-        assert(actual, expected, `Expected: ${expected}, Actual: ${actual}`);
+        assert(!isPalindrome(-121));
     });
     it('test_02', function(){
-        const input = 10;
-        const expected = false;
-        const actual = isPalindrome(input);
-        assert(actual, expected, `Expected: ${expected}, Actual: ${actual}`);
+        assert(!isPalindrome(10));
     });
 });
