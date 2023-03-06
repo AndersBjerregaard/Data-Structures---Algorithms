f58108aeadc1077dc26279eb0d42fc77658c6970
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2023-03-06 18:38:10 +0100

    wip: optPalindrome test passing
    The optimized implementation of the isPalindrome function does not pass test cases correctly, when the input is of 5 digits or higher

--- NodeJs src/IsPalindrome.js
+++ NodeJs src/IsPalindrome.js
@@ -20,17 +20,44 @@ var isPalindrome = function(x) {
     return xArray.toString() == yArray.toString();
 };
 
+// Inspiration:
+// https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript/28203456#28203456
+// https://stackoverflow.com/questions/13955738/javascript-get-the-second-digit-from-a-number
 /**
  * @param {number} x
  * @return {boolean}
  */
-// https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript/28203456#28203456
-// https://stackoverflow.com/questions/13955738/javascript-get-the-second-digit-from-a-number
 var isPalindromeOpt = function(x) {
+    if (x < 0) return false;
     var numDigits = Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
-    function getDigit(number, n) {
-        return Math.floor((number / Math.pow(10, n - 1)) % 10);
+    if (numDigits === 1) return true;
+    const map = {};
+    var isOdd = numDigits % 2 > 0;
+    var halfLength = Math.round(numDigits / 2);
+    if (isOdd) {
+        for (let i = 1; i < numDigits + 1; i++) {
+            var digit = Math.floor((x / Math.pow(10, i - 1)) % 10);
+            if (i < halfLength) {
+                map[digit] = true;
+            } else if (i === halfLength) {
+                continue;
+            }
+            else {
+                if (map[digit] === undefined) return false;
+            }
+        }
+    } else {
+        for (let i = 1; i < numDigits + 1; i++) {
+            var digit = Math.floor((x / Math.pow(10, i - 1)) % 10);
+            if (i <= halfLength) {
+                map[digit] = true;
+            }
+            else {
+                if (map[digit] === undefined) return false;
+            }
+        }
     }
+    return true;
 };
 
 // Tests
@@ -47,6 +74,15 @@ describe('IsPalindrome.js tests', function(){
     it('test_02', function(){
         assert.equal(isPalindrome(10), false);
     });
+    it('test_03', function(){
+        assert.equal(isPalindrome(11), true);
+    });
+    it('test_04', function(){
+        assert.equal(isPalindrome(1001), true);
+    });
+    it('test_05', function(){
+        assert.equal(isPalindrome(123123), false);
+    });
 });
 describe('IsPalindrome.js opt tests', function(){
     it('test_00', function(){
@@ -57,5 +93,14 @@ describe('IsPalindrome.js opt tests', function(){
     });
     it('test_02', function(){
         assert.equal(isPalindromeOpt(10), false);
+    });
+    it('test_03', function(){
+        assert.equal(isPalindromeOpt(11), true);
+    });
+    it('test_04', function(){
+        assert.equal(isPalindromeOpt(1001), true);
+    });
+    it('test_05', function(){
+        assert.equal(isPalindromeOpt(123123), false);
     });
 });
