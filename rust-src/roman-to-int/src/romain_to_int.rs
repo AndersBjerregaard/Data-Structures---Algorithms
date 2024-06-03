dc14c70d859a256ede49fb9ac046ba86bf8fa17f
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2024-06-03 20:14:11 +0200

    lazy static

--- rust-src/roman-to-int/src/romain_to_int.rs
+++ rust-src/roman-to-int/src/romain_to_int.rs
@@ -15,17 +15,17 @@
 pub mod romain_to_int {
     use std::collections::HashMap;
 
-	lazy_static! {
-		static ref ROMAN_LETTERS: HashMap<&'static str, i32> = {
-		let mut m = HashMap::new();
-		m.insert("I", 1);
-		m.insert("V", 5);
-		m.insert("X", 10);
-		m.insert("L", 50);
-		m.insert("C", 100);
-		m.insert("D", 500);
-		m.insert("M", 1000);
-		};
+    lazy_static! {
+        static ref ROMAN_LETTERS: HashMap<&'static str, i32> = {
+        let mut m = HashMap::new();
+            m.insert("I", 1);
+            m.insert("V", 5);
+            m.insert("X", 10);
+            m.insert("L", 50);
+            m.insert("C", 100);
+            m.insert("D", 500);
+            m.insert("M", 1000);
+        };
     }
 
     pub fn romain_to_int(x: &str) -> u32 {
