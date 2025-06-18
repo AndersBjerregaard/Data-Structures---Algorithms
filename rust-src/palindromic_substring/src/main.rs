520071ba1104ac2935f542a06821d8debfba1abb
Anders Nielsen <an@betterboard.dk>
2025-06-18 07:51:34 +0200

    boilerplate for longest_palindrome.rs

--- rust-src/palindromic_substring/src/main.rs
+++ rust-src/palindromic_substring/src/main.rs
@@ -1,3 +1,45 @@
+/*
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+*/
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn longest_palindrome(s: String) -> String {
+    todo!()
+}
+
+#[test]
+fn test1() {
+    let input: String = String::from("babad");
+    let output: String = longest_palindrome(input);
+    let expected: String = String::from("bab");
+    assert_eq!(output, expected);
+}
+
+#[test]
+fn test2() {
+    let input: String = String::from("cbbd");
+    let output: String = longest_palindrome(input);
+    let expected: String = String::from("bb");
+    assert_eq!(output, expected);
 }
